dffa7d001a4b85fdb949162e08fb6ebd2f70271a
Pranav Waingade <pranavwaingade25@gmail.com>
2026-06-12 19:13:05 +0530

    added some new

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './Components/Navbar'
-import Hero from './Components/Hero'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-
   return (
-  <>
-    <Navbar></Navbar>
-    <br />
-    <Hero></Hero>
-  </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Hero from '../Components/Hero'
+import Footer from '../Components/Footer'
+
+function Home() {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
