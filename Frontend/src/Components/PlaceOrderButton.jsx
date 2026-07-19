84b8769421e4d8510c2f129647f2c0fcfe777bd9
Pranav Waingade <pranavwaingade25@gmail.com>
2026-07-19 19:43:49 +0530

    Checkout Page Done

--- Frontend/src/Components/PlaceOrderButton.jsx
+++ Frontend/src/Components/PlaceOrderButton.jsx
@@ -5,7 +5,7 @@ const PlaceOrderButton = () => {
   const navigate =useNavigate
   return (
     <div style={{width:'99%',paddingTop:'5px'}}>
-              <button onClick={()=>{navigate('/')}}  style={{ background: " #A46C45", color: "white", border: "none", borderRadius: "10px", fontSize: '18px', fontWeight: 'bold', cursor: 'pointer', transition: '.3s', width: '100%', padding: '5px' }}>Place Order</button>
+              <button onClick={()=>{navigate('/cart')}}  style={{ background: " #A46C45", color: "white", border: "none", borderRadius: "10px", fontSize: '18px', fontWeight: 'bold', cursor: 'pointer', transition: '.3s', width: '100%', padding: '5px' }}>Place Order</button>
     </div>
   )
 }
--- Frontend/src/Pages/Checkout.jsx
+++ Frontend/src/Pages/Checkout.jsx
@@ -1,46 +1,39 @@
-import React from 'react'
-import CheckoutAddress from '../Components/CheckoutAddress'
-import PaymentMethod from '../Components/PaymentMethod'
-import CheckoutSummary from '../Components/CheckoutSummary'
-import PlaceOrderButton from '../Components/PlaceOrderButton'
-import Navbar from '../Components/Navbar'
+import Navbar from "../Components/Navbar";
+import CheckoutAddress from "../Components/CheckoutAddress";
+import PaymentMethod from "../Components/PaymentMethod";
+import CheckoutSummary from "../Components/CheckoutSummary";
+import PlaceOrderButton from "../Components/PlaceOrderButton";
+import "../Style/Checkout.css";
 
 const Checkout = () => {
-    return (
-        <div style={{paddingBottom:'50px'}}>
-            <Navbar />
-            <div style={{ paddingTop: '75px', height: '150px', flexDirection: 'column', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#8b451389' }}>
-                <h1>CheckOut</h1>
-                <p style={{ backgroundColor: '#ffffff00', color: 'white', fontSize: 'x-large' }}>You have 4 items in your cart.</p>
-            </div>
-            <div style={{ display: 'flex',paddingTop:'10px', width: '100%', justifyContent: 'center', height: 'fit-content', alignContent: 'center', gap: '15px' }}>
-                <div style={{
-                    display: 'flex', flexDirection: 'column', height: 'fit-content', padding: '15px', width: '40%'
-                    ,textAlign: "start",
-                    padding: "10px",
-                    border: "1px solid #ddd",
-                    borderRadius: "20px",
-                    background: "#fff",
-                    boxShadow: "0 8px 20px rgba(0,0,0,0.08)",
-                }}>
-                    <CheckoutAddress />
-                    <PaymentMethod />
-                </div>
-                <div style={{ display: 'flex', flexDirection: 'column', height: 'fit-content', padding: '15px', width: '25%', border: '1px solid black',textAlign: "start",
-                    padding: "10px",
-                    border: "1px solid #ddd",
-                    borderRadius: "20px",
-                    background: "#fff",
-                    boxShadow: "0 8px 20px rgba(0,0,0,0.08)",justifyContent:'flex-start',alignItems:'center' }}>
-                    <CheckoutSummary />
-                    <PlaceOrderButton />
-                </div>
+  return (
+    <>
+      <Navbar />
 
-            </div>
+      <div className="checkout-page">
 
+        <div className="checkout-header">
+          <h1>Checkout</h1>
+          <p>You have 4 items in your cart.</p>
+        </div>
+
+        <div className="checkout-container">
+
+          <div className="checkout-left">
+            <CheckoutAddress />
+            <PaymentMethod />
+          </div>
+
+          <div className="checkout-right">
+            <CheckoutSummary />
+            <PlaceOrderButton />
+          </div>
 
         </div>
-    )
-}
 
-export default Checkout
+      </div>
+    </>
+  );
+};
+
+export default Checkout;
