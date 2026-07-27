d39ca1a4b57cbceb0cef26ac0369fd3e8defeda1
Pranav Waingade <pranavwaingade25@gmail.com>
2026-07-27 21:15:03 +0530

    Data Base connection

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,38 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
 const app = express();
 
 
-// Middleware
+// Connect Database
+connectDB();
+
+
+// Middlewares
 app.use(cors());
+
 app.use(express.json());
 
 
 // Test Route
 app.get("/", (req, res) => {
-    res.send("CupsCraft Backend Server is Running 🚀");
+
+    res.send(
+        "CupsCraft Backend Server is Running 🚀"
+    );
+
 });
 
 
 // Server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
+
     console.log(
-        `Server running on port ${PORT}`
+        `Server running on port ${PORT} 🚀`
     );
 
 });
